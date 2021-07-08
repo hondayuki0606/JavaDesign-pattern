2b5baab7593aa5e6b1e0c118904c34b4a11ebae0
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-07-08 09:02:36 +0900

    演習問題3-1から3-4

--- app/src/main/java/com/example/designpattern/templatemethod/AbstractDisplay.java
+++ app/src/main/java/com/example/designpattern/templatemethod/AbstractDisplay.java
@@ -2,9 +2,13 @@ package com.example.designpattern.templatemethod;
 
 abstract public class AbstractDisplay {
 
-    abstract void open();
-    abstract void close();
-    public abstract void print();
+    // 問題3−3 アクセス権限をprotectedにする
+    // 問題3−4 displayの実体メソッドを記述しいてるため
+    protected abstract void open();
+    protected abstract void close();
+    protected abstract void print();
+
+    // 問題3−2 Finalを記述することでサブクラスではオーバーライドさせないようにしている
     public final void display() {
         open();
         for(int i=0 ; i<5; i++) {
