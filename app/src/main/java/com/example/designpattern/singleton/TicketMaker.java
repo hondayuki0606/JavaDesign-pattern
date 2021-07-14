aa29a8d6acfdf26e8ce4ba44e12b2a87adf5d56c
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-07-14 10:40:49 +0900

    シングルトン　演習問題5−１

--- app/src/main/java/com/example/designpattern/singleton/TicketMaker.java
+++ app/src/main/java/com/example/designpattern/singleton/TicketMaker.java
@@ -2,9 +2,14 @@ package com.example.designpattern.singleton;
 
 //演習問題5−１
 public class TicketMaker {
-    private static TicketMaker ticketMaker = new TicketMaker();
+    private static TicketMaker singleton = new TicketMaker();
     private int tickect = 1000;
-    public int getNextTicketNumber() {
+    private TicketMaker() {
+    }
+    public static TicketMaker getInstance(){
+        return singleton;
+    }
+    public synchronized int getNextTicketNumber() {
         return tickect++;
     }
 }
