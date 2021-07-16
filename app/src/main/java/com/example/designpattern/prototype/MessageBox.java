1a7fbbaff36660ba5e5f2bc7e9610dca4858f36e
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-07-16 09:03:44 +0900

    MessageBox UndelinePenの更新

--- app/src/main/java/com/example/designpattern/prototype/MessageBox.java
+++ app/src/main/java/com/example/designpattern/prototype/MessageBox.java
@@ -7,6 +7,16 @@ public class MessageBox implements Product{
     }
     public void use(String s) {
 
+        int length = s.getBytes().length;
+        for (int i = 0; i < length + 4; i++) {
+            System.out.print(decochar);
+        }
+        System.out.println("");
+        System.out.println(decochar + " " + s + " " + decochar);
+        System.out.print(" ");
+        for (int i = 0; i < length + 4; i++) {
+            System.out.print(decochar);
+        }
         System.out.println("");
     }
     public Product createClone() {
--- app/src/main/java/com/example/designpattern/prototype/UnderlinePen.java
+++ app/src/main/java/com/example/designpattern/prototype/UnderlinePen.java
@@ -7,6 +7,12 @@ public class UnderlinePen implements Product{
     }
     public void use(String s) {
 
+        int length = s.getBytes().length;
+        System.out.println("/" + s + "/");
+        System.out.print(" ");
+        for (int i = 0; i < length; i++) {
+            System.out.print(ulchar);
+        }
         System.out.println("");
     }
     public Product createClone() {
