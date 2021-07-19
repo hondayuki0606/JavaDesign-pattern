d2c4ccbb690a618ca3e3a67f146c3af547afe277
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-07-19 11:15:15 +0900

    Builderパターンの実装

--- app/src/androidTest/java/com/example/designpattern/BuilderTest.java
+++ app/src/androidTest/java/com/example/designpattern/BuilderTest.java
@@ -1,19 +1,14 @@
 package com.example.designpattern;
 
 import android.content.Context;
-import android.text.Html;
-
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
-
 import com.example.designpattern.builder.Director;
 import com.example.designpattern.builder.HTMLBuilder;
 import com.example.designpattern.builder.TextBuilder;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.assertEquals;
-
 
 @RunWith(AndroidJUnit4.class)
 public class BuilderTest {
