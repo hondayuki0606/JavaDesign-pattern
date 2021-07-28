b2f99201a1f84a4e15855970865ab646f1f00fe6
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-07-29 08:48:22 +0900

    getPath

--- app/src/main/java/com/example/designpattern/abstractfactory/tablefactory/TableFactory.java
+++ app/src/main/java/com/example/designpattern/abstractfactory/tablefactory/TableFactory.java
@@ -4,21 +4,17 @@ import com.example.designpattern.abstractfactory.factory.Factory;
 import com.example.designpattern.abstractfactory.factory.Link;
 import com.example.designpattern.abstractfactory.factory.Page;
 import com.example.designpattern.abstractfactory.factory.Tray;
-import com.example.designpattern.abstractfactory.listfactory.ListLink;
-import com.example.designpattern.abstractfactory.listfactory.ListPage;
-import com.example.designpattern.abstractfactory.listfactory.ListTray;
 
-public class TableFactory {
+public class TableFactory extends Factory {
+    public Link createLink(String caption, String url) {
+        return new TableLink(caption, url);
+    }
 
-    public class TableFactory extends Factory {
-        public Link createLink(String caption, String url) {
-            return new TableLink(caption, url);
-        }
-        public Tray createTray(String caption) {
-            return new TableTray(caption);
-        }
-        public Page createPage(String title, String author) {
-            return new TablePage(title, author);
-        }
+    public Tray createTray(String caption) {
+        return new TableTray(caption);
+    }
+
+    public Page createPage(String title, String author) {
+        return new TablePage(title, author);
     }
 }
